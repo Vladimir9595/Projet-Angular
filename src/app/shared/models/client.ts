ad795504d0d5aa448f814641689cf628029304f4
Vladimir9595 <vladisac@hotmail.it>
2023-12-31 19:01:45 +0100

    fix : modify model Client

--- src/app/shared/models/client.ts
+++ src/app/shared/models/client.ts
@@ -1,9 +1,25 @@
 export class Client {
-  firstname: string;
   lastname: string;
+  firstname: string;
+  adress: string;
+  postalcode: string;
+  city: string;
+  email: string;
+  sex: string;
+  login: string;
+  password: string;
+  phonenumber: string;
 
-  constructor(firstname: string, lastname: string) {
-    this.firstname = firstname;
-    this.lastname = lastname;
+  constructor() {
+    this.lastname = '';
+    this.firstname = '';
+    this.adress = '';
+    this.postalcode = '';
+    this.city = '';
+    this.email = '';
+    this.sex = '';
+    this.login = '';
+    this.password = '';
+    this.phonenumber = '';
   }
 }
