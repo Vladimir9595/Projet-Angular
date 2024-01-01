10b41c8c5e17d62bb05bc262f227667236549f1c
Vladimir9595 <vladisac@hotmail.it>
2024-01-01 20:47:23 +0100

    feat : add payement

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { CartState } from '../../shared/states/cart.state';
-import {
-  RemoveFromCart,
-  ClearCart,
-} from '../../shared/actions/cart.actions';
+import { RemoveFromCart, ClearCart } from '../../shared/actions/cart.actions';
 import { Product } from '../../shared/models/product';
 
 @Component({
@@ -14,6 +11,7 @@ import { Product } from '../../shared/models/product';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  message: string = '';
   @Select(CartState.cartItems) cartItems$: Observable<Product[]> | undefined;
 
   constructor(private store: Store) {}
@@ -24,5 +22,6 @@ export class CartComponent {
 
   clearCart() {
     this.store.dispatch(new ClearCart());
+    this.message = 'Payement effectué avec succès';
   }
 }
