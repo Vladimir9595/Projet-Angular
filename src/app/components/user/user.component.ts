defa1c5d6f1b61bdb4dd991083cd69cbf95893bc
Vladimir SACCHETTO <vladimir.sacchetto@al-enterprise.com>
2024-01-02 16:29:02 +0100

    feat : add verifications form insert

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ProductService } from '../../services/product.service';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+import { ClientService } from '../../services/client.service';
 import { Client } from '../../shared/models/client';
 
 @Component({
@@ -16,25 +21,34 @@ export class UserComponent {
   lastname: string = '';
   message: string = '';
 
-  constructor(private productService: ProductService, private fb: FormBuilder) {
+  constructor(private clientService: ClientService, private fb: FormBuilder) {
     this.userForm = this.fb.group({
-      lastname: ['', Validators.required],
-      firstname: ['', Validators.required],
-      adress: ['', Validators.required],
-      postalcode: ['', Validators.required],
-      city: ['', Validators.required],
-      email: ['', Validators.required],
+      lastname: [
+        '',
+        [Validators.required, this.noNumbersOrSpecialCharsValidator],
+      ],
+      firstname: [
+        '',
+        [Validators.required, this.noNumbersOrSpecialCharsValidator],
+      ],
+      adress: [
+        '',
+        [Validators.required, this.allowedCharactersValidatorAddress],
+      ],
+      postalcode: ['', [Validators.required, Validators.pattern('[0-9]{5}')]],
+      city: ['', [Validators.required, this.noNumbersOrSpecialCharsValidator]],
+      email: ['', [Validators.required, Validators.email]],
       sex: ['', Validators.required],
-      login: ['', Validators.required],
+      login: ['', [Validators.required, this.allowedCharactersValidatorLogin]],
       password: ['', Validators.required],
-      phonenumber: ['', Validators.required],
+      phonenumber: ['', [Validators.required, this.onlyNumbers]],
     });
   }
 
   createUser(): void {
     if (this.userForm.valid) {
       const client: Client = this.userForm.value;
-      this.productService.createUser(client).subscribe(
+      this.clientService.createUser(client).subscribe(
         (data) => {
           console.log('Utilisateur ajouté avec succès', data);
           this.userForm.reset();
@@ -48,10 +62,73 @@ export class UserComponent {
           this.message = "Erreur lors de l'ajout de l'utilisateur";
         }
       );
+    } else {
+      this.message = 'Veuillez remplir tous les champs correctement.';
     }
   }
 
   togglePasswordVisibility(): void {
     this.hidePassword = !this.hidePassword;
+  }
+
+  noNumbersOrSpecialCharsValidator(
+    control: AbstractControl
+  ): { [key: string]: any } | null {
+    const hasNumbersOrSpecialChars = /[0-9~#{}\[\]()`"'&$£%*.,;:\\/]/.test(
+      control.value
+    );
+    return hasNumbersOrSpecialChars ? { noNumbersOrSpecialChars: true } : null;
+  }
+
+  allowedCharactersValidatorLogin(
+    control: AbstractControl
+  ): { [key: string]: any } | null {
+    const allowedCharactersLogin = /^[a-zA-Z0-9_]*$/.test(control.value);
+    return allowedCharactersLogin ? null : { invalidCharactersLogin: true };
+  }
+
+  allowedCharactersValidatorAddress(
+    control: AbstractControl
+  ): { [key: string]: any } | null {
+    const allowedCharactersAddress =
+      /^[a-zA-Z0-9,âêîôûäëïöüàæçéèœùÂÊÎÔÛÄËÏÖÜÀÆÇÉÈŒÙ\s]*$/.test(control.value);
+    return allowedCharactersAddress ? null : { invalidCharactersAddress: true };
+  }
+
+  onlyNumbers(control: AbstractControl): { [key: string]: any } | null {
+    const onlyNumbers = /^[0-9]*$/.test(control.value);
+    return onlyNumbers ? null : { invalidCharactersPhone: true };
+  }
+
+  get firstnameControl() {
+    return this.userForm.get('firstname');
+  }
+
+  get lastnameControl() {
+    return this.userForm.get('lastname');
+  }
+
+  get addressControl() {
+    return this.userForm.get('adress');
+  }
+
+  get cityControl() {
+    return this.userForm.get('city');
+  }
+
+  get postalcodeControl() {
+    return this.userForm.get('postalcode');
+  }
+
+  get emailControl() {
+    return this.userForm.get('email');
+  }
+
+  get loginControl() {
+    return this.userForm.get('login');
+  }
+
+  get phonenumberControl() {
+    return this.userForm.get('phonenumber');
   }
 }
