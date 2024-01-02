4b807556b82903da3b7faaebbf90cfa8ec0cf65b
Vladimir SACCHETTO <vladimir.sacchetto@al-enterprise.com>
2024-01-02 16:27:49 +0100

    fix  : clear product service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,28 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Product } from '../shared/models/product';
-import { Client } from '../shared/models/client';
 import { environment } from '../environments/environment';
 
 @Injectable()
 export class ProductService {
   private productsUrl = environment.backendCatalogue;
   private createProductUrl = environment.backendCreateProduit;
-  private createUserUrl = environment.backendCreateUser;
-  private loginUrl = environment.backendLoginClient;
-
+  
   constructor(private http: HttpClient) {}
-
-  public loginClient(login: string, password: string): Observable<Client> {
-    let data: String;
-    let httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
-      }),
-    };
-    data = 'login=' + login + '&password=' + password;
-    return this.http.post<Client>(this.loginUrl, data, httpOptions);
-  }
 
   public createProduct(product: Product): Observable<Product> {
     const formData = new FormData();
@@ -34,22 +20,6 @@ export class ProductService {
     formData.append('category', product.category);
 
     return this.http.post<Product>(this.createProductUrl, formData);
-  }
-
-  public createUser(client: Client): Observable<Client> {
-    const formData = new FormData();
-    formData.append('lastname', client.lastname);
-    formData.append('firstname', client.firstname);
-    formData.append('adress', client.adress);
-    formData.append('postalcode', client.postalcode.toString());
-    formData.append('city', client.city);
-    formData.append('email', client.email);
-    formData.append('sex', client.sex);
-    formData.append('login', client.login);
-    formData.append('password', client.password);
-    formData.append('phonenumber', client.phonenumber.toString());
-
-    return this.http.post<Client>(this.createUserUrl, formData);
   }
 
   public getProducts(): Observable<Product[]> {
